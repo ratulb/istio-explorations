e82f415eb1f93090351e40ecafd5fb2d498c6680
Ratul Buragohain <ratul75@hotmail.com>
2021-06-29 23:31:48 +0000

    Traffic policy

--- routing-1/src/main/java/istio/routing/RoutingV1Application.java
+++ routing-1/src/main/java/istio/routing/RoutingV1Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.InetAddress;
+
 @RestController
 @SpringBootApplication
 
+
 public class RoutingV1Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoutingV1Application.class, args);
@@ -15,7 +19,7 @@ public class RoutingV1Application {
 
 	@GetMapping("/")
 	public String index(){
-		return "\nindex - version v1\n";
+		return "\nindex - version v1:  "+ RoutingV1Application()+"\n";
 	}
 
 	@GetMapping("/service1")
@@ -34,5 +38,15 @@ public class RoutingV1Application {
 	@ResponseBody
 	public String catchAll() {
 		return "\nCatch all requests - version v1\n";
+	}
+
+	private static String RoutingV1Application(){
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			return  ip.toString();
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
--- routing-2/src/main/java/istio/routing/RoutingV2Application.java
+++ routing-2/src/main/java/istio/routing/RoutingV2Application.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.net.InetAddress;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
@@ -19,7 +21,7 @@ public class RoutingV2Application {
 
 	@GetMapping("/")
 	public String index(){
-		return "\nindex - version v2\n";
+		return "\nindex - version v2:  "+ RoutingV1Application()+"\n";
 	}
 
 	@GetMapping("/service1")
@@ -45,5 +47,15 @@ public class RoutingV2Application {
 	public String headerRoute(HttpServletRequest request) {
 		System.out.println("The user agent: " + request.getHeader("user-agent"));
 		return "\nRequest routing  based on header \"release=rc1\"\n";
+	}
+	private static String RoutingV1Application(){
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			return ip.toString();
+
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
